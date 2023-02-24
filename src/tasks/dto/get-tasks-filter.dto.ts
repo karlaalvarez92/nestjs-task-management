8b3056249dbb1c8e7eb9bb31d736a792e43e87e3
Karla Elizabeth Alvarez Castillo <karla.alvarez@michelada.io>
2023-02-24 13:14:58 -0600

    Add validation pipes to tasks requests

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,12 @@
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 import { TaskStatus } from 'src/tasks/task.model';
 
 export class GetTasksFilterDto {
-  status: TaskStatus;
-  search: string;
+  @IsOptional()
+  @IsEnum(TaskStatus)
+  status?: TaskStatus;
+
+  @IsOptional()
+  @IsString()
+  search?: string;
 }
