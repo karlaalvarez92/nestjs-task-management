e59a96d679ed9a2b57bff468b69618a20ecff84f
Karla Elizabeth Alvarez Castillo <karla.alvarez@michelada.io>
2023-02-27 13:25:46 -0600

    Update Repository from typeorm-nestjs documentation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksModule } from './tasks/tasks.module';
+import { DataSource } from 'typeorm';
 
 @Module({
   imports: [
@@ -17,4 +18,6 @@ import { TasksModule } from './tasks/tasks.module';
     }),
   ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
