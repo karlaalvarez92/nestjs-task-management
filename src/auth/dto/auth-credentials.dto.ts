6885a68839c5a3a0582a486110f7d7cbbfd2aabf
Karla Elizabeth Alvarez Castillo <karla.alvarez@michelada.io>
2023-03-07 17:23:21 -0600

    Add authentication module

---
+++ src/auth/dto/auth-credentials.dto.ts
@@ -0,0 +1,17 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
+export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  username: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(32)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'Must contain at least one number, one uppercase and lowercase letter, and at least 8 or more characters',
+  })
+  password: string;
+}
